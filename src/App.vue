bc1ffc6d833cbe553644285788f3b4d27c42bed2
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-07-08 04:38:12 -0500

    Documentos avanzados

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+  <nav class ="navbar navbar-dark bg.primary">
+  <a href="/" class="navbar-brand">Vuejs Firebase Database</a> 
+  </nav>
+  <!-- Main content-->
+  <div class="container">
+    <div class="row mt-5">
+    <div class="col-sm-4">
+      <div class="card">
+      <div class="card-header">
+      <h3>Add a new Webside</h3>
+        </div>
+        <div class="card-body">
+        <form>
+
+        </form>
+
+        </div>
+        </div>
+        </div>
+        <div class="col-sm-8 text-center">
+    <img src="./assets/logo.png">
+
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App' 
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
