e4df26179a41baf5ce2d8dadd15ad48d6eee5fdd
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-07-10 02:28:34 -0500

    Tabla

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,27 @@
         </div>
         <div class="col-sm-8 text-center">
     <img src="./assets/logo.png">
+      <div class="card">
+        <div class="card-header">
+        <h3>Websites List</h3>
+        </div>
+        <div class="card-body">
+        <table class="table  table-striped table-bordered">
+        
+        <thead>
+        <tr>
+        <th>Name</th>
+        <th>Author</th>
+        <th>Operations</th>
+        </tr>
+        </thead>
+        
+        <tbody>
 
-          </div>
+        </tbody>
+
+        </div>
+         </div>
         </div>
       </div>
   </div>
